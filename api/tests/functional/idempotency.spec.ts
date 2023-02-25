584f51aa568f878aafff64f522a3102f53fb4ca8
Nick Abbene <njabbene@gmail.com>
2023-02-24 19:46:47 -0500

    feat: Check that request without idempotency key does not create a db record

--- api/tests/functional/idempotency.spec.ts
+++ api/tests/functional/idempotency.spec.ts
@@ -1,31 +1,54 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 import { v4 as uuid } from 'uuid'
+import IdempotentRequest from 'App/Models/IdempotentRequest'
 
-test('request with the same idempotent key return the same response', async ({
-  client,
-  expect,
-}) => {
-  const idempotencyKey = uuid()
-  const response = await client.post('/authorizations').header('X-Idempotency-Key', idempotencyKey)
-  const response2 = await client.post('/authorizations').header('X-Idempotency-Key', idempotencyKey)
+test.group('Idempotency Tests', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.each.teardown(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
 
-  const firstResponseId = response.body()['id']
-  const secondResponseId = response2.body()['id']
+  test('request with the same idempotency key return the same response', async ({
+    client,
+    expect,
+  }) => {
+    const idempotencyKey = uuid()
+    const response = await client
+      .post('/authorizations')
+      .header('X-Idempotency-Key', idempotencyKey)
+    const response2 = await client
+      .post('/authorizations')
+      .header('X-Idempotency-Key', idempotencyKey)
 
-  response.assertStatus(200)
-  expect(firstResponseId).toEqual(secondResponseId)
-})
+    const firstResponseId = response.body()['id']
+    const secondResponseId = response2.body()['id']
 
-test('request with the different idempotent key does not return the same response', async ({
-  client,
-  expect,
-}) => {
-  const response = await client.post('/authorizations').header('X-Idempotency-Key', uuid())
-  const response2 = await client.post('/authorizations').header('X-Idempotency-Key', uuid())
+    response.assertStatus(200)
+    expect(firstResponseId).toEqual(secondResponseId)
+  })
 
-  const firstResponseId = response.body()['id']
-  const secondResponseId = response2.body()['id']
+  test('request with the different idempotency key does not return the same response', async ({
+    client,
+    expect,
+  }) => {
+    const response = await client.post('/authorizations').header('X-Idempotency-Key', uuid())
+    const response2 = await client.post('/authorizations').header('X-Idempotency-Key', uuid())
 
-  response.assertStatus(200)
-  expect(firstResponseId).not.toEqual(secondResponseId)
+    const firstResponseId = response.body()['id']
+    const secondResponseId = response2.body()['id']
+
+    response.assertStatus(200)
+    expect(firstResponseId).not.toEqual(secondResponseId)
+  })
+
+  test('request without idempotency key does not touch database', async ({ client, expect }) => {
+    const response = await client.post('/authorizations')
+    const idempotentRequest = await IdempotentRequest.first()
+
+    expect(idempotentRequest).toBeNull()
+    response.assertStatus(200)
+  })
 })
