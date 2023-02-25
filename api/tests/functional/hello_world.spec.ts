67ebe8ed787e4ed055d095e7cf742b2da5441306
Nick Abbene <njabbene@gmail.com>
2023-02-24 19:46:46 -0500

    set up tests

--- api/tests/functional/hello_world.spec.ts
+++ api/tests/functional/hello_world.spec.ts
@@ -1,6 +1,13 @@
 import { test } from '@japa/runner'
 
-test('display welcome page', async ({ client }) => {
+test('test1', async ({ client }) => {
+  const response = await client.get('/')
+
+  response.assertStatus(200)
+  response.assertBodyContains({ hello: 'world' })
+})
+
+test('test2', async ({ client }) => {
   const response = await client.get('/')
 
   response.assertStatus(200)
--- docker-config/postgres-dev-init.sql
+++ docker-config/postgres-dev-init.sql
@@ -1,3 +1,6 @@
 CREATE USER adonis with encrypted password 'adonis';
 CREATE DATABASE adonis_app;
 GRANT ALL PRIVILEGES ON DATABASE adonis_app TO adonis;
+
+CREATE DATABASE test;
+GRANT ALL PRIVILEGES ON DATABASE test TO adonis;
