65e14350579e8be23d2ca39476e1cb3993f54e29
Nick Abbene <njabbene@gmail.com>
2023-03-06 22:13:29 -0500

    feat: Setup external api and 'nonproduction' toy server

---
+++ external/start/routes.ts
@@ -0,0 +1,53 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+import axios from 'axios'
+
+Route.post('/type1', async ({ request, response }) => {
+  const webhookDestination = request.header('X-Callback-Url')
+  if (webhookDestination) {
+    await axios.post(webhookDestination, { id: request.body().id })
+    return response.ok({ message: 'success' })
+  }
+  return response.badRequest({ message: 'Missing X-Callback-Url - is it spelled correctly?' })
+})
+
+Route.post('/type2', async ({ request, response }) => {
+  const metadata = request.body().metadata
+  if (!metadata) {
+    return response.badRequest({ message: 'Missing metadata - is it spelled correctly?' })
+  }
+  // This webhook is typically configured in some type of Admin Panel. In this case, we're just going to hardcode.
+  await axios.post('http://nonproduction_instance:3340/webhook', {
+    id: request.body().id,
+    metadata: metadata,
+  })
+  return response
+})
+
+Route.post('/type3', async ({ request, response }) => {
+  if (!request.hasBody() || !request.body().id) {
+    return response.badRequest({ message: 'Missing body or missing ID field' })
+  }
+  // This webhook is typically configured in some type of Admin Panel. In this case, we're just going to hardcode.
+  await axios.post('http://nonproduction_instance:3340/webhook', { id: request.body().id })
+  return { id: request.body().id }
+})
